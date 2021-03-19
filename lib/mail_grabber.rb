ef00521a68fee8f6cb7832e736cabb63a9b4602a
Norbert Szivos <sysqa@yahoo.com>
2021-03-19 05:20:19 +0100

    Generate skeleton files of the gem

---
+++ lib/mail_grabber.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "mail_grabber/version"
+
+module MailGrabber
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/mail_grabber_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe MailGrabber do
+  it "has a version number" do
+    expect(MailGrabber::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
